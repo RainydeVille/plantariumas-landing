6d8bf842c2900fa14a28895bb6614a4d5e19528a
RainydeVille <vaitkevicienegle@gmail.com>
2026-04-20 14:43:10 +0300

    Moved styles to scss and implemented custom logo font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "../styles/globals.css";
+import "../styles/globals.scss";
+import "../styles/fonts.scss";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",

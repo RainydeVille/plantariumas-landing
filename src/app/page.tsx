7382633ac43a3279b1b88881afeb1d41809b5f92
RainydeVille <vaitkevicienegle@gmail.com>
2026-04-20 15:26:09 +0300

    bare nav bar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+import Navbar from "@/components/Navbar";
+
 export default function Home() {
   return (
-    <main>
-      <h1 id="logoFont">Plantariumas</h1>
-    </main>
+    <>
+      <Navbar />
+      <main>
+        <h1 id="logoFont">Tis were main stuff goes</h1>
+      </main>
+    </>
   );
 }
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,35 @@
+import styles from "./Navbar.module.scss";
+import Link from "next/link";
+
+export default function Navbar() {
+  return (
+    <header className={styles.header}>
+      <nav className={styles.nav}>
+        <ul className={styles.navLeft}>
+          <li>
+            <Link href="/">Pagrindinis</Link>
+          </li>
+          <li>
+            <Link href="/apie">Apie</Link>
+          </li>
+          <li>
+            <Link href="/duk">DUK</Link>
+          </li>
+        </ul>
+
+        <h1 id="logoFont" className={styles.logo}>
+          Plantariumas
+        </h1>
+
+        <ul className={styles.navRight}>
+          <li>
+            <Link href="/prisijungti">Prisijungti</Link>
+          </li>
+          <li>
+            <Link href="/registruotis">Registruotis</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}

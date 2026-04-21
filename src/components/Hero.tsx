15eff236493dd2049d18bb2f9673a99784a21621
RainydeVille <vaitkevicienegle@gmail.com>
2026-04-21 13:21:59 +0300

    Animated hero images with framer motion

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,6 +2,7 @@
 
 import { useEffect, useState } from "react";
 import styles from "./Hero.module.scss";
+import { AnimatePresence, motion } from "framer-motion";
 
 //image array
 const images = ["/images/hero1.jpg", "/images/hero2.jpg", "/images/hero3.jpg"];
@@ -13,13 +14,24 @@ export default function Hero() {
   useEffect(() => {
     const interval = setInterval(() => {
       setIndex((prev) => (prev + 1) % images.length);
-    }, 120000); //2 minutes
+    }, 40000); //2 minutes
 
     return () => clearInterval(interval);
   }, []);
 
   return (
-    <section className={styles.hero} style={{ backgroundImage: `url(${images[index]})` }}>
+    <section className={styles.hero}>
+      <AnimatePresence>
+        <motion.div
+          key={index}
+          className={styles.bg}
+          style={{ backgroundImage: `url(${images[index]})` }}
+          initial={{ opacity: 0, scale: 0.95 }}
+          animate={{ opacity: 1, scale: 1 }}
+          exit={{ opacity: 0, scale: 1.05 }}
+          transition={{ duration: 2 }}
+        />
+      </AnimatePresence>
       <div className={styles.overlay}>
         <div className={styles.heroContent}>
           <h2 className={styles.heroTitle}>Rūpintis savo augalais dar niekada nebuvo taip lengva 💚</h2>

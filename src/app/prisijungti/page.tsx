8cc248f1f1465e89ff61890cb43b356b5fa15c69
RainydeVille <vaitkevicienegle@gmail.com>
2026-04-27 13:34:25 +0300

    FAQ and SignIn minimal pages

---
+++ src/app/prisijungti/page.tsx
@@ -0,0 +1,12 @@
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+
+export default function SignInForm() {
+  return (
+    <>
+      <Navbar />
+      <main id="main"></main>
+      <Footer />
+    </>
+  );
+}

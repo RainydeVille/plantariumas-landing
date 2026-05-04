e5a65dc77a49608a28e64d0e635602291d686084
RainydeVille <vaitkevicienegle@gmail.com>
2026-05-04 12:09:50 +0300

    updated footers z-index, made registration form look nicer

--- src/components/Registration.tsx
+++ src/components/Registration.tsx
@@ -2,11 +2,22 @@ import styles from "./Registration.module.scss";
 
 export default function Registration() {
   return (
-    <form className={styles.form}>
-      <input type="email" placeholder="el. paštas" required />
-      <input type="password" placeholder="slaptažodis" required />
+    <div className={styles.formWrapper}>
+      <form className={styles.form}>
+        <h2 className={styles.title}>Smagu, kad nusprendėte prie mūsų prisijungti! 🪴</h2>
+        <label className={styles.label} htmlFor="email">
+          El. paštas:
+        </label>
+        <input className={styles.input} id="email" name="email" type="email" placeholder="el. paštas" required />
+        <label className={styles.label} htmlFor="password">
+          Slaptažodis:
+        </label>
+        <input className={styles.input} id="password" name="password" type="password" placeholder="slaptažodis" required />
 
-      <button type="submit">Sukurti paskyrą</button>
-    </form>
+        <button className={styles.btn} type="submit">
+          Sukurti paskyrą
+        </button>
+      </form>
+    </div>
   );
 }
--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -3,6 +3,7 @@ import styles from "./SignIn.module.scss";
 export default function SignIn() {
   return (
     <form className={styles.form}>
+      <h2>Džiaugiamės matydami tave vėl! 🪴</h2>
       <input type="email" placeholder="el. paštas" required />
       <input type="password" placeholder="slaptažodis" required />
 

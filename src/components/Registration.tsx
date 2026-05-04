f1755960e0aad5b3db30e6c131b3b5c28cc90fb3
RainydeVille <vaitkevicienegle@gmail.com>
2026-05-04 15:46:09 +0300

    Better registration form

--- src/components/Registration.tsx
+++ src/components/Registration.tsx
@@ -5,18 +5,20 @@ export default function Registration() {
     <div className={styles.formWrapper}>
       <form className={styles.form}>
         <h2 className={styles.title}>Smagu, kad nusprendėte prie mūsų prisijungti! 🪴</h2>
-        <label className={styles.label} htmlFor="email">
-          El. paštas:
-        </label>
-        <input className={styles.input} id="email" name="email" type="email" placeholder="el. paštas" required />
-        <label className={styles.label} htmlFor="password">
-          Slaptažodis:
-        </label>
-        <input className={styles.input} id="password" name="password" type="password" placeholder="slaptažodis" required />
+        <div className={styles.formContent}>
+          <label className={styles.label} htmlFor="email">
+            El. paštas:
+          </label>
+          <input className={styles.input} id="email" name="email" type="email" placeholder="el. paštas" required />
+          <label className={styles.label} htmlFor="password">
+            Slaptažodis:
+          </label>
+          <input className={styles.input} id="password" name="password" type="password" placeholder="slaptažodis" required />
 
-        <button className={styles.btn} type="submit">
-          Sukurti paskyrą
-        </button>
+          <button className={styles.btn} type="submit">
+            Sukurti paskyrą
+          </button>
+        </div>
       </form>
     </div>
   );
